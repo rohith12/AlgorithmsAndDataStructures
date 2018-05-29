0ae8c8829682f8cf9a973a3def51feafd501f76b
Rohith <r12.rohith@gmail.com>
2018-05-29 08:54:18 -0400

    Initial Commit

---
+++ AlgorithmsAndDataStructures/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  AlgorithmsAndDataStructures
+//
+//  Created by Rohith Raju on 29/05/18.
+//  Copyright © 2018 Rohith Raju. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
